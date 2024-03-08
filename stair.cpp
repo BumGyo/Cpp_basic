4da11e0f9cc994e313f43ffd6c53291129095d0b
BumGyo <89345976+BumGyo@users.noreply.github.com>
2024-03-08 10:48:57 +0900

    Add files via upload

---
+++ stair.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+	int i, j = 0;
+
+	for (i = 1; i <= 4; i++) {
+		for (j = 1; j <= i; j++) {
+			std::cout << "*";
+		}
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
